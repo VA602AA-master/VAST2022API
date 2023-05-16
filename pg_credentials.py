9f3270eb800a429e1b3b654fe49c2bb1f0bdd8c8
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2023-05-16 17:57:05 +0200

    setting environment variables for connection

---
+++ pg_credentials.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SA_USER = os.getenv("SA_USER")
+SA_PASSWORD = os.getenv("SA_PASSWORD")
+SA_HOST = os.getenv("SA_HOST")
+SA_PORT = os.getenv("SA_PORT")
+SA_DATABASE = os.getenv("SA_DATABASE")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{SA_USER}:{SA_PASSWORD}@{SA_HOST}:{SA_PORT}/{SA_DATABASE}"
